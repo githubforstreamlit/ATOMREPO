65a4a52080216f5c31f00efcac7e7c1a2ecf6e4d
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-05-12 18:41:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,7 +73,7 @@ async def pm_text(bot, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=False)
     try:
         offset = int(offset)
     except:
